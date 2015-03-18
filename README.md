a78f55cb544ef2755b81f669c4cd9aab1c92032e
Michael Crosby <crosbymichael@gmail.com>
2015-03-18 00:20:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,125 @@
+# zkctl - zookeeper cli client
+
+`zkctl` is a easy to use cli to zookeeper that allows you to 
+create, list, get, set, and delete nodes within zookeeper.
+
+
+```bash
+NAME:
+   zkctl - cli appliction for zookeeper
+
+USAGE:
+   zkctl [global options] command [command options] [arguments...]
+
+VERSION:
+   1
+
+AUTHOR:
+  @crosbymichael - <crosbymichael@gmail.com>
+
+COMMANDS:
+   create   create a node
+   set      set a key's value
+   get      get a key's value
+   ls       list nodes
+   delete   delete a key
+   help, h  Shows a list of commands or help for one command
+   
+GLOBAL OPTIONS:
+   --debug, -D              enable debug output
+   -t, --timeout "2s"           connection timeout
+   --zk [--zk option --zk option]   zookeeper node addresses
+   --help, -h               show help
+   --version, -v            print the version
+   
+```
+
+
+### Usage:
+
+#### Create a new node
+
+```bash
+NAME:
+   create - create a node
+
+USAGE:
+   command create [command options] [arguments...]
+
+OPTIONS:
+   -v, --version "0"                node version
+   --world, -w                  set world ACL
+   --acl, -a [--acl option --acl option]    ACL used on create. default is all. all|create|write|read|delete|admin
+   
+
+zkctl create /name koye
+```
+
+#### Get a node's value
+```
+NAME:
+   get - get a key's value
+
+USAGE:
+   command get [arguments...]
+
+
+zkctl get /name
+koye
+```
+
+#### Set the value for an exist node
+
+Automatically get the node's version for ease of use in changing the value by using 
+the `--auto` flag.  You can also use the `-v` flag to set the version to a specific
+value.
+
+```
+NAME:
+   set - set a key's value
+
+USAGE:
+   command set [command options] [arguments...]
+
+OPTIONS:
+   -v, --version "0"    node version
+   --auto       automatically set the version based on the current node
+   
+zkctl set --auto /name mike
+```
+
+#### List nodes
+
+```
+NAME:
+   ls - list nodes
+
+USAGE:
+   command ls [arguments...]
+
+
+zkctl ls /
+/mesos
+/name
+/zookeeper
+/marathon
+```
+
+#### Delete a node
+
+`delete` also accepts `--auto` and `-v` for version handling.
+
+```
+NAME:
+   delete - delete a key
+
+USAGE:
+   command delete [command options] [arguments...]
+
+OPTIONS:
+   -v, --version "0"    node version
+   --auto       automatically set the version based on the current node
+   
+
+zkctl delete --auto /name
+```
---
+++ main.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
+	"github.com/samuel/go-zookeeper/zk"
+)
+
+var (
+	zookeeper *zk.Conn
+	setFlags  = []cli.Flag{
+		cli.IntFlag{Name: "v,version", Value: 0, Usage: "node version"},
+	}
+	permMap = map[string]int32{
+		"all":    zk.PermAll,
+		"write":  zk.PermWrite,
+		"read":   zk.PermRead,
+		"create": zk.PermCreate,
+		"delete": zk.PermDelete,
+		"admin":  zk.PermAdmin,
+	}
+)
+
+// getPath ensures that the path provided on the cli has "/" prepended.
+func getPath(context *cli.Context) string {
+	return filepath.Join("/", context.Args().Get(0))
+}
+
+// getACL returns the correct zk.ACL values specified on the cli.
+// it defaults to PermAll if none are provided.
+func getACL(context *cli.Context) []zk.ACL {
+	var aclFlag int32
+	for _, perm := range context.StringSlice("acl") {
+		aclFlag |= permMap[perm]
+	}
+	if aclFlag == 0 {
+		aclFlag = zk.PermAll
+	}
+	return zk.WorldACL(aclFlag)
+}
+
+var createCommand = cli.Command{
+	Name:  "create",
+	Usage: "create a node",
+	Flags: append(setFlags,
+		cli.BoolFlag{Name: "world,w", Usage: "set world ACL"},
+		cli.StringSliceFlag{Name: "acl,a", Value: &cli.StringSlice{}, Usage: "ACL used on create. default is all. all|create|write|read|delete|admin"},
+	),
+	Action: func(context *cli.Context) {
+		if _, err := zookeeper.Create(
+			getPath(context),
+			[]byte(context.Args().Get(1)),
+			int32(context.Int("version")),
+			getACL(context),
+		); err != nil {
+			logrus.Error(err)
+			zookeeper.Close()
+			os.Exit(1)
+		}
+	},
+}
+
+var setCommand = cli.Command{
+	Name:  "set",
+	Usage: "set a key's value",
+	Flags: append(setFlags, cli.BoolFlag{Name: "auto", Usage: "automatically set the version based on the current node"}),
+	Action: func(context *cli.Context) {
+		var (
+			path    = getPath(context)
+			version = int32(context.Int("version"))
+		)
+		// if auto fetch the current value for the node so that we
+		// get the stats and version to use when setting the new value.
+		if context.Bool("auto") {
+			_, s, err := zookeeper.Get(path)
+			if err != nil {
+				logrus.Error(err)
+				zookeeper.Close()
+				os.Exit(1)
+			}
+			version = s.Version
+		}
+		if _, err := zookeeper.Set(
+			path,
+			[]byte(context.Args().Get(1)),
+			version,
+		); err != nil {
+			logrus.Error(err)
+			zookeeper.Close()
+			os.Exit(1)
+		}
+	},
+}
+
+var lsCommand = cli.Command{
+	Name:  "ls",
+	Usage: "list nodes",
+	Action: func(context *cli.Context) {
+		path := getPath(context)
+		children, _, err := zookeeper.Children(path)
+		if err != nil {
+			logrus.Error(err)
+			zookeeper.Close()
+			os.Exit(1)
+		}
+		for _, c := range children {
+			fmt.Println(filepath.Join(path, c))
+		}
+	},
+}
+
+var getCommand = cli.Command{
+	Name:  "get",
+	Usage: "get a key's value",
+	Action: func(context *cli.Context) {
+		r, s, err := zookeeper.Get(getPath(context))
+		if err != nil {
+			logrus.Error(err)
+			zookeeper.Close()
+			os.Exit(1)
+		}
+		logrus.WithFields(logrus.Fields{"version": s.Version}).Debug("node stats")
+		fmt.Printf("%s", r)
+	},
+}
+
+var deleteCommand = cli.Command{
+	Name:  "delete",
+	Usage: "delete a key",
+	Flags: append(setFlags, cli.BoolFlag{Name: "auto", Usage: "automatically set the version based on the current node"}),
+	Action: func(context *cli.Context) {
+		var (
+			path    = getPath(context)
+			version = int32(context.Int("version"))
+		)
+		// if auto fetch the current value for the node so that we
+		// get the stats and version to use when setting the new value.
+		if context.Bool("auto") {
+			_, s, err := zookeeper.Get(path)
+			if err != nil {
+				logrus.Error(err)
+				zookeeper.Close()
+				os.Exit(1)
+			}
+			version = s.Version
+		}
+		if err := zookeeper.Delete(path, version); err != nil {
+			logrus.Error(err)
+			zookeeper.Close()
+			os.Exit(1)
+		}
+	},
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "zkctl"
+	app.Usage = "cli appliction for zookeeper"
+	app.Version = "1"
+	app.Author = "@crosbymichael"
+	app.Email = "crosbymichael@gmail.com"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{Name: "debug,D", Usage: "enable debug output"},
+		cli.DurationFlag{Name: "t,timeout", Value: 2 * time.Second, Usage: "connection timeout"},
+		cli.StringSliceFlag{Name: "zk", Value: &cli.StringSlice{}, Usage: "zookeeper node addresses"},
+	}
+	app.Commands = []cli.Command{
+		createCommand,
+		setCommand,
+		getCommand,
+		lsCommand,
+		deleteCommand,
+	}
+	app.Before = func(context *cli.Context) error {
+		if context.GlobalBool("debug") {
+			logrus.SetLevel(logrus.DebugLevel)
+		}
+		servers := context.GlobalStringSlice("zk")
+		if len(servers) == 0 {
+			servers = []string{"127.0.0.1"}
+		}
+		conn, _, err := zk.Connect(servers, context.Duration("timeout"))
+		zookeeper = conn
+		return err
+	}
+	app.After = func(context *cli.Context) error {
+		zookeeper.Close()
+		return nil
+	}
+	if err := app.Run(os.Args); err != nil {
+		logrus.Fatal(err)
+	}
+}
